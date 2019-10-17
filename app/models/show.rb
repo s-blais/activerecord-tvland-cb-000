16d4dd80fdd3e1dbc61e802f3ddea201e9c09b0b
Stephen Blais <stephen@s-blais.com>
2019-10-17 20:41:27 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+
+  def actors_list
+    self.actors.collect do |actor|
+      actor.full_name
+    end
+  end
 end
